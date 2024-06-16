f771f652535f56a47557b83717ca7a18ef97e516
Franco4848 <francoalvarez4848@gmail.com>
2024-06-16 20:58:58 -0300

    aniadir enfermo funcionando, formulario de html conectado con bd

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask_mysqldb import MySQLdb
+from flask_mysqldb import MySQL
 from dotenv import load_dotenv
 import os
 
@@ -10,6 +10,34 @@ app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_CURSORCLASS'] = os.getenv('MYSQL_CURSORCLASS')
+
+myslq = MySQL(app)
+
+@app.route('/')
+def view_add_sick():
+    return render_template ('add_sick.html')
+
+@app.route('/add_sick', methods=['POST'])
+def add_sick():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        dni = request.form['dni']
+        provincia = request.form['provincia']
+        departamento = request.form['departamento']
+        barrio = request.form['barrio']
+        calle = request.form['calle']
+        numeracion = request.form['numeracion']
+        caso = request.form['caso']
+        
+        cur = myslq.connection.cursor()
+        cur.execute('''INSERT INTO enfermos (nombre, apellido, dni, provincia, 
+        departamento, barrio, calle, numeracion, caso) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)''',
+        (nombre,apellido,dni,provincia,departamento	,barrio,calle,numeracion,caso))
+        
+        myslq.connection.commit()
+    return 'enfermo cargado'
+
 
 
 
